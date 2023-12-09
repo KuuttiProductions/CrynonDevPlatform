b620f56eecaf99bf847b96494e945555377d0943
KudeKube <Kuutti.taavitsainen@icloud.com>
2023-12-09 21:05:45 +0200

    Initial Commit

---
+++ CrynonDevPlatform/CrynonDevPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrynonDevPlatformApp.swift
+//  CrynonDevPlatform
+//
+//  Created by Kuutti Taavitsainen on 9.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CrynonDevPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
